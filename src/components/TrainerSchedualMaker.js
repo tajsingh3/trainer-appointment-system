3f23b9452ef6e2e9548a23dd6aec64a0da6e6e72
tajsingh3 <tajsingh3@gmail.com>
2019-02-25 17:26:08 -0500

    added all datepicker stuff

--- src/components/TrainerSchedualMaker.js
+++ src/components/TrainerSchedualMaker.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import moment from 'moment';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import moment from "moment";
+
+import "react-dates/initialize";
+import {
+  DateRangePicker,
+  SingleDatePicker,
+} from "react-dates";
+import "react-dates/lib/css/_datepicker.css";
 
 const styles = theme => ({
   container: {
-    display: 'flex',
-    flexWrap: 'wrap',
+    display: "flex",
+    flexWrap: "wrap"
   },
   textField: {
     marginLeft: theme.spacing.unit,
     marginRight: theme.spacing.unit,
-    width: 200,
-  },
+    width: 200
+  }
 });
 
 function TimePickers(props) {
@@ -21,33 +28,40 @@ function TimePickers(props) {
 
   return (
     //<form className={classes.container} noValidate>
+    <div>
+        <SingleDatePicker />
       <TextField
         id="time"
-        label="Alarm clock"
+        label="Appointment Time"
         type="time"
         defaultValue="07:30"
         className={classes.textField}
         InputLabelProps={{
-          shrink: true,
+          shrink: true
         }}
         inputProps={{
-          step: 300, // 5 min
+          step: 300 // 5 min
         }}
-        onChange={(e) => {
-            let val = e.target.value;
-            let now = moment();
-            let fullDate = `${now.format('YYYY-MM-DD')} ${val}:00:00`;
+        onChange={e => {
+          let val = e.target.value;
+          let now = moment();
+          let fullDate = `${now.format("YYYY-MM-DD")} ${val}:00:00`;
 
-            let fullMoment = moment(fullDate);
-            console.log(fullMoment.format('YYYY-MM-DD H:mm'));
+          let fullMoment = moment(fullDate);
+          console.log(fullMoment.format("YYYY-MM-DD H:mm"));
         }}
       />
+      <DateRangePicker
+  
+/>
+    </div>
+
     //</form>
   );
 }
 
 TimePickers.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(TimePickers);
