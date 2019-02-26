16aca318e8b05f4a9fe63a84cb343c00694bd4b8
tajsingh3 <tajsingh3@gmail.com>
2019-02-26 13:32:04 -0500

    added header list

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,14 +2,10 @@ import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
-import Button from "@material-ui/core/Button";
 import List from "@material-ui/core/List";
-import Divider from "@material-ui/core/Divider";
 import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
-import InboxIcon from "@material-ui/icons/MoveToInbox";
-import MailIcon from "@material-ui/icons/Mail";
+import { Link } from "react-router-dom";
 
 import Header from "./Header";
 
@@ -41,36 +37,24 @@ class TemporaryDrawer extends React.Component {
 
     const sideList = (
       <div className={classes.list}>
-        {/* <List>
-          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List> */}
-        {/* <Divider />
         <List>
-          {["All mail", "Trash", "Spam"].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List> */}
-        <List>
-            <ListItem>My Appointments</ListItem>
+          <ListItem button>
+            {/* <Link ><ListItemText primary="My Appointments" /></Link> */}
+            <ListItemText primary="My Appointments" />
+          </ListItem>
+          <ListItem button>
+            <ListItemText primary="Create Appointments Slots" />
+          </ListItem>
+          <ListItem button>
+            <ListItemText primary="Available Appointments" />
+          </ListItem>
         </List>
       </div>
     );
 
     return (
       <div>
-        <Header toggleDrawer={this.toggleDrawer("left", true)}/>
+        <Header toggleDrawer={this.toggleDrawer("left", true)} />
         <Drawer
           open={this.state.left}
           onClose={this.toggleDrawer("left", false)}
